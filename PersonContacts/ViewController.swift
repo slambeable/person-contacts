927eef7801fc079f65b2763f10d25d99712e32d4
slambeable <41022916+slambeable@users.noreply.github.com>
2022-01-14 13:35:33 +0300

    Initial Commit

---
+++ PersonContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonContacts
+//
+//  Created by Андрей Евдокимов on 14.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
